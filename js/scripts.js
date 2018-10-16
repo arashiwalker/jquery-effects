8af3c3578c02c231a8d1e55c91d3795cad6960ae
arashi walker and Victoria Martinez <arashi@>
2018-10-16 10:30:48 -0700

    background color with clickable functions

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,29 @@ $(document).ready(function() {
     $(".walrus-hidden").fadeToggle();
   });
 
-$(".clickable2").click(function() {
-  $(".cat-showing").fadeToggle();
-  $(".cat-hidden").fadeToggle();
-});
+  $(".clickable2").click(function() {
+    $(".cat-showing").fadeToggle();
+    $(".cat-hidden").fadeToggle();
+  });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
+
 });
